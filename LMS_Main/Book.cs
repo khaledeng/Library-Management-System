a1740ca2193eb52643b07aa81424d6804aca122a
dell <mohammedeldeeb550@gmail.com>
2026-01-21 20:35:47 +0200

    Add_Class_Book

---
+++ LMS_Main/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Book
+    {
+        public int ID;
+        public string Title;
+        public string Author;
+        public bool Availability = true;
+
+        public Book(int iD, string title, string author)
+        {
+            ID = iD;
+            Title = title;
+            Author = author;
+
+        }
+        public override string ToString()
+        {
+            return $"Book Id : {ID} Book Title : {Title} Book Author : {Author} Book Availability :{Availability}";
+
+        }
+
+    }
+}
