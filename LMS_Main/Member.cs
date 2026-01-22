6fbc216bba508ec0b6249c77fb127e1f57c14d82
khaled ahmed <khaled.pro.963@gmail.com>
2026-01-22 20:24:14 +0200

    Create Member Class

--- LMS_Main/Member.cs
+++ LMS_Main/Member.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using project;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LMS_Main
 {
     internal class Member
     {
+        public int Id;
+        public string Name;
+        public List<Book> BorrowedBooks;
+
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            BorrowedBooks = new List<Book>();
+        }
+
+        public void BorrowBook(Book book)
+        {
+            BorrowedBooks.Add(book);
+        }
+
+        public bool ReturnBook(int bookId)
+        {
+            for (int i = 0; i < BorrowedBooks.Count; i++)
+            {
+                if (BorrowedBooks[i].ID == bookId)
+                {
+                    BorrowedBooks.RemoveAt(i);//
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
