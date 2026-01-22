a170969134dce295be384d29969414ecd3d43a4f
khaled ahmed <khaled.pro.963@gmail.com>
2026-01-22 14:05:57 +0200

    Try program

--- LMS_Main/Program.cs
+++ LMS_Main/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace LMS_Main
+﻿using project;
+
+namespace LMS_Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Book book = new Book(1,"OOP","Mostafa");
+            Console.WriteLine(book);
         }
     }
 }
